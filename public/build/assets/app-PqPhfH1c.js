bea38819b4af3ceb564eacb270c397c0b44e1bc0
musta20 <must.g20@gmail.com>
2024-07-01 17:20:20 +0300

    fix vite & classes name

---
+++ public/build/assets/app-PqPhfH1c.js
@@ -0,0 +1,1 @@
+import{m as a,a as l}from"./module.esm-DZs5scYj.js";window.Alpine=a;a.plugin(l);a.start();
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,11 @@ export default defineConfig({
             input: [
         'resources/css/app.css',
         'resources/css/slide.css',
+        'resources/assets/css/style.css',
+        'resources/assets/js/app.js',
         'resources/css/style.css',
+        'resources/assets/css/style.css',
+        'resources/css/model.css',
         'resources/js/app.js',
             ],
             refresh: true,
