af71dbc0a816fc43637de48cd711762fc5b39c41
musta20 <must.g20@gmail.com>
2024-08-09 22:25:10 +0300

    styling login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,54 +5,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('icons/css/all.min.css') }}">
-    <link href="{{ asset('Dashboard/app-saas-rtl.min.css') }}" rel="stylesheet" type="text/css" id="app-style">
+    @vite(['resources/css/style.css', 'resources/js/app.js'])
+    {{--
+    <link rel="stylesheet" href="{{ asset('icons/css/all.min.css') }}"> --}}
+    {{--
+    <link href="{{ asset('Dashboard/app-saas-rtl.min.css') }}" rel="stylesheet" type="text/css" id="app-style"> --}}
     <title>تسجيل الدخول</title>
 </head>
 
-<body>
+<body class="m-0 bg-slate-100">
     <section class="mainBody">
 
-        <main class="loginCard border">
-            <form method="POST" class="loginform w-50" action="{{ url('/login') }}">
+        <main class="loginCard text-[#6B7280] font-Noto flex w-5/6 md:w-1/2  m-24 mx-auto bg-white rounded-md border">
+            <form method="POST" class="loginform w-full md:w-1/2 flex flex-col p-5 gap-5 place-content-center" action="{{ url('/login') }}">
                 @csrf
-                <h4>تسجيل الدخول
+                <h4 class="text-xl font-bold">تسجيل الدخول
                     <hr>
                 </h4>
                 @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <p><strong>حدث خطاء</strong></p>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
+                <div class="alert alert-danger">
+                    <p><strong>حدث خطاء</strong></p>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
                 @endif
-                <div class="mb-3">
+                <div class="mb-3 flex flex-col gap-3">
                     <label class="form-label">البريد الالكتروني </label>
-                    <input class="form-control  " type="email" name="email" placeholder=" البريد الالكتروني " />
-                    {{--  @error('email')
-                        <span class="helper">
-                            {{ $message }}
-                        </span>
+                    <input class="form-control  rounded-md p-2 border" type="email" name="email" placeholder=" البريد الالكتروني " />
+                    {{-- @error('email')
+                    <span class="helper">
+                        {{ $message }}
+                    </span>
                     @enderror --}}
                 </div>
-                <div class="mb-3">
+                <div class="mb-3 flex flex-col gap-3">
                     <label class="form-label"> كلمة المرور</label>
-                    <input class="form-control  " type="password" name="password" placeholder=" كلمة المرور" />
-                    {{--    @error('password')
-                        <span class="helper">
-                            {{ $message }}
-                        </span>
+                    <input class="form-control rounded-md p-2 border " type="password" name="password" placeholder=" كلمة المرور" />
+                    {{-- @error('password')
+                    <span class="helper">
+                        {{ $message }}
+                    </span>
                     @enderror --}}
                 </div>
 
-                <div class="mb-3">
+                {{-- <div class="mb-3">
                     <input class="  " type="checkbox" name="remember" />
                     <label class="form-label">تذكرني </label>
-                </div>
-  {{--               <div class="mb-3">
+                </div> --}}
+                {{-- <div class="mb-3">
                     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
                     <div class="g-recaptcha" id="feedback-recaptcha" data-sitekey="{{ env('GOOGLE_RECAPTCHA_KEY') }}">
                     </div>
@@ -60,18 +63,27 @@
                 </div> --}}
 
                 <div class="mb-3">
-                    <button type="submit" class="btn btn-primary">تسجيل الدخول</button>
+                    <button type="submit" class="btn btn-primary  w-full">تسجيل الدخول</button>
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class=" rounded-full border p-2 flex justify-evenly gap-2  w-full">
+                         
+                          الدخول بواسطة جوجل 
+                        
+
+                        <img class="fill" width="25" src="{{ Vite::asset('resources/logo/google-icon-logo-svgrepo-com.svg') }}" />
+                    </button>
                 </div>
             </form>
-            <div class="backLogin w-50">
-                <img class="fill" src="{{ asset('storage/logo/logo.png') }}" />
+            <div class="backLogin w-1/2 hidden md:flex">
+                <img class="fill" src="{{  Vite::asset('resources/logo/logo.png') }}" />
             </div>
         </main>
     </section>
 </body>
 
 </html>
-<style>
+{{-- <style>
     .loginCard {
         background-color: white;
         border-radius: 15px;
@@ -122,4 +134,4 @@
         /*         background-image: linear-gradient(to left, rgb(220, 220, 220),#87afd8);
  */
     }
-</style>
+</style> --}}
